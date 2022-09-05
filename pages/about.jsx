42fd2e7f955f7a044240dcf67fa59491feee3b0e
Ayush <thapaaayush115@gmail.com>
2022-09-05 21:24:20 +0545

    description updated

--- pages/about.jsx
+++ pages/about.jsx
@@ -24,7 +24,7 @@ export default function About() {
     
     <div className={styles.aboutc}>
 
-        <p>The bike shop in Pokhara, Nepal. Chamling Company's Running Adventures Cycling Tours / Trips. Aslo MTB Rides, Sale, Rental & Bicycles Maintenance. And giving much better services to customers. Not only that our team all born for cycling / MTB. We have more than 10 years experience guides mechanics and riders staffs. And also the founder self 15 years experienced in Mountain Biking in all sectors.</p>
+        <p>The bike shop in Pokhara, Nepal. Chamling Company is Running Adventures Cycling Tours / Trips. Aslo MTB Rides, Sale, Rental & Bicycles Maintenance. And giving much better services to customers. Not only that our team all born for cycling / MTB. We have more than 10 years experience guides mechanics and riders staffs. And also the founder self 15 years experienced in Mountain Biking in all sectors.</p>
     </div>
     
     </Template>  
